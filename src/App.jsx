b7ac0d680d3524ce6260f3d0e27d8ca2edce8bd0
healrabbit <alphasorell@gmail.com>
2025-11-03 16:17:39 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import "./App.css";
+import { useState, useRef } from 'react';
+import html2canvas from 'html2canvas';
+const initialImages = [
+  { id: 1, url: "https://picsum.photos/100?1", title: "One"},
+  {id: 2, url: "https://picsum.photos/100?2", title: "Two"},
+  {id: 3, url: "https://picsum.photos/100?3", title: "Three"}, 
+  { id: 4, url: "https://picsum.photos/100?4", title: "Four"},
+  {id: 5, url: "https://picsum.photos/100?5", title: "Five"},
+  {id: 6, url: "https://picsum.photos/100?6", title: "Six"}, 
+]
+
+export default function Gallery(){
+const [images, setImages] = useState(initialImages);
+const [draggedId, setDraggedId] = useState(null);
+const [hoveredId, setHoveredId] = useState(null);
+const galleryRef = useRef(null);
+const handleDragStart = (id) => setDraggedId(id);
+
+//below is function to switch images when dropped 
+const handleDrop = (targetId) => {
+
+
+  if (draggedId === targetId) return;
+
+const draggedIndex = images.findIndex (i => i.id === draggedId); //find index of image thats being dragged
+const targetIndex = images.findIndex(i => i.id === targetId); //find index of image thats hovered over
+
+const newImages = [...images]; // '...' is copy of images array and assigned to "newImages"
+
+const [moved] = newImages.splice(draggedIndex, 1); //splice cuts dragged image out of copy
+newImages.splice(targetIndex, 0, moved); //splice putting dragged image into array in "targetIndex" spot
+// update real arrays and reset dragged and hovered states to empty
+setImages(newImages);
+setDraggedId(null);
+setHoveredId(null);
+};
+
+  const handleUpload = (e) => {
+    //make array from uploaded files
+    const files = Array.from(e.target.files)
+    //set info for all added images
+    const newImages = files.map((file,index) => ({
+      id:  Date.now() + index,
+      url: URL.createObjectURL(file),
+      title: file.name,
+
+    }))
+    //updating images to include new images
+    setImages([...images, ...newImages])
+    }
+
+const downloadGallery = async () =>  {
+  //if gallery in DOM (loaded)
+  if(!galleryRef.current) return;
+//creating canvas element from gallery html
+  const canvas = await html2canvas(galleryRef.current, { useCORS: true})
+
+  //convert canvas to png
+  const dataURL = canvas.toDataURL("image/png");
+
+  //trigger download
+  const link = document.createElement("a");
+
+  link.href= dataURL;
+  link.download = "gallery.png";
+  link.click();
+
+}
+
+  const removeImage = (id) => {
+  
+    setImages(images.filter((img) => img.id !== id))
+       console.log(images.filter((img) => img.id !== id))
+  }
+
+return(
+  <div>
+    <input type="file"
+    multiple
+    onChange= {handleUpload}/> 
+
+    <button onClick={downloadGallery}>
+Download your image set! 
+    </button>
+  <div className="gallery" ref={galleryRef}> 
+  {images.map(img => (
+    <div key = {img.id}
+    draggable
+    onDragStart = {() => handleDragStart(img.id)}
+    onDragOver = {(e) => {
+e.preventDefault(); //e is js object, checking what default behavior is (stopping from being dragged ) and preventing it 
+    }}
+    onDragLeave = {() => setHoveredId(null)}
+    onDrop = {() => handleDrop(img.id)}
+    className = {`image-wrapper ${draggedId == img.id ? "dragged" : ""} 
+    ${hoveredId == img.id && draggedId != img.id ? "hovered" : ""}`}
+    >
+      
+    <img src={img.url} alt={img.title}/>
+  <button className="remove-btn" 
+     onClick={() => removeImage(img.id)}> x  </button>
+
+    </div>
+  ))}
+  </div>
+  </div>
+)
+}
